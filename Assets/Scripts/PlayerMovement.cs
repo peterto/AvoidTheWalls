e718788343f87cbeccb8fd737f9991bc4a558628
Peter To <pt9386@gmail.com>
2016-08-09 18:39:12 -0400

    Revert "Merge pull request #3 from peterto/platform-dependent"
    This reverts commit df8be2dc52ea6e8283ba50074e4b41f2bd284279, reversing
    changes made to b94aeab17e417c00e9aa7666b094741c9d3fb86c.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,6 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-    private float lastClickTime;
-
-    public float catchTime = 0.25f;
-
-
 	[SerializeField] float _xspeed = 10f;
 	[SerializeField] float _yspeed = -100f;
 	[SerializeField] float _playerSpeed = 10f;
@@ -24,19 +19,12 @@ public class PlayerMovement : MonoBehaviour {
 	[SerializeField] GameObject _pickUps;
 	[SerializeField] GameObject _menuController;
 	public static bool _runAnimation = true;
-
-    private Vector3 _endPosition;
-    private Vector3 _startPosition;
-//    public GameObject thingToMove;
-//    public float smooth = 2;
-	
-    void Start () {
+	void Start () {
 		pickUpCount = 0;
 
 		if (this.gameObject.GetComponent<Rigidbody2D> () == null) {
 			this.gameObject.AddComponent<Rigidbody2D> ();
 		}
-//        thingToMove = this.gameObject;
 	}
 	
 	// Update is called once per frame
@@ -45,58 +33,38 @@ public class PlayerMovement : MonoBehaviour {
 		if (!_isDead) {
 
 
-//            #if UNITY_IOS
-//                if (this.gameObject.GetComponent<Rigidbody2D> () == null) {
-//                    this.gameObject.AddComponent<Rigidbody2D> ();
-//                } else {
-//                    Rigidbody2D rigidbody = this.gameObject.GetComponent<Rigidbody2D> ();
-//                    rigidbody.AddForce (new Vector2 (0, 0));
-//                if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
-//                        Vector2 pos = Input.GetTouch(0).position;
-//                        pos = Camera.main.ScreenToWorldPoint(pos);
-//                        Vector2 newPos = new Vector2(pos.x, this.transform.position.y);
-//                        transform.position = pos;
-//                    }
-//                }
-//            #elif UNITY_STANDALONE
-//
-//    			float horizontal = Input.GetAxisRaw ("Horizontal");
-//    			if (this.gameObject.GetComponent<Rigidbody2D> () == null) {
-//    				this.gameObject.AddComponent<Rigidbody2D> ();
-//    			} else {
-//    				Rigidbody2D rigidbody = this.gameObject.GetComponent<Rigidbody2D> ();
-//    				rigidbody.velocity = Vector2.right * horizontal * _xspeed;
-//    				rigidbody.AddForce (new Vector2 (0, _yspeed));
-//    				if (Input.GetKey (KeyCode.DownArrow) || Input.GetMouseButton (0)) {
-//    					_isSuper = true;
-//    					rigidbody.AddForce (new Vector2 (0, _yspeed * _playerSpeed));
-//    				} else {
-//    					_isSuper = false;
-//    				}
-//    			}
-//            #endif
-//            /* in line input controls
-          if (this.gameObject.GetComponent<Rigidbody2D> () == null) {
-              this.gameObject.AddComponent<Rigidbody2D> ();
-          } else {
-                Rigidbody2D rigidbody = this.gameObject.GetComponent<Rigidbody2D> ();
-//                rigidbody.gravityScale = 0;
-                rigidbody.AddForce (new Vector2 (0, _yspeed));
+            #if UNITY_IOS
+                if (this.gameObject.GetComponent<Rigidbody2D> () == null) {
+                    this.gameObject.AddComponent<Rigidbody2D> ();
+                } else {
+                    Rigidbody2D rigidbody = this.gameObject.GetComponent<Rigidbody2D> ();
+                    rigidbody.AddForce (new Vector2 (0, 0));
+                if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+//                        Vector3 pos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.GetTouch(0).position.x, this.transform.position.y, 0));
+//                        transform.position = new Vector3(pos.x, pos.y, pos.z);
+                        Vector2 pos = Input.GetTouch(0).position;
+                        pos = Camera.main.ScreenToWorldPoint(pos);
+                        Vector2 newPos = new Vector2(pos.x, this.transform.position.y);
+                        transform.position = pos;
+                    }
+                }
+            #elif UNITY_STANDALONE
 
-//                if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer) {
-//                    _endPosition = HandleTouchInput();
-//                } else {
-                    _endPosition = HandleMouseInput();
-//                }
-                Vector3 newPos = this.transform.position;
-                newPos.x = _endPosition.x;
-                rigidbody.velocity = new Vector2(0, 0);
-//                thingToMove.transform.position = Vector3.Lerp(thingToMove.transform.position, new Vector3(_endPosition.x, _endPosition.y, 0), Time.deltaTime * smooth);
-                this.transform.position = newPos;
-
-
-            }
-//            */
+    			float horizontal = Input.GetAxisRaw ("Horizontal");
+    			if (this.gameObject.GetComponent<Rigidbody2D> () == null) {
+    				this.gameObject.AddComponent<Rigidbody2D> ();
+    			} else {
+    				Rigidbody2D rigidbody = this.gameObject.GetComponent<Rigidbody2D> ();
+    				rigidbody.velocity = Vector2.right * horizontal * _xspeed;
+    				rigidbody.AddForce (new Vector2 (0, _yspeed));
+    				if (Input.GetKey (KeyCode.DownArrow) || Input.GetMouseButton (0)) {
+    					_isSuper = true;
+    					rigidbody.AddForce (new Vector2 (0, _yspeed * _playerSpeed));
+    				} else {
+    					_isSuper = false;
+    				}
+    			}
+            #endif
 
 		} else {
 	
@@ -112,42 +80,6 @@ public class PlayerMovement : MonoBehaviour {
 
 		}
 	}
-
-    private Vector3 HandleTouchInput() {
-        for (var i = 0; i < Input.touchCount; i++) {
-            if (Input.GetTouch(i).phase == TouchPhase.Began) {
-                Vector2 screenPosition = Input.GetTouch(i).position;
-                _startPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-//                if (Input.touchCount == 2) {
-//                    Rigidbody2D rigidbody = this.gameObject.GetComponent<Rigidbody2D> ();
-//                    rigidbody.AddForce (new Vector2 (0, _yspeed * _playerSpeed));
-//                }
-            }
-        }
-
-        return _startPosition;
-    }
-
-    private Vector3 HandleMouseInput() {
-        if(Input.GetButtonDown("Fire1")) {
-            if(Time.time - lastClickTime < catchTime) {
-                //double click
-                print("Double click");
-                Rigidbody2D rigidbody = this.GetComponent<Rigidbody2D>();
-                rigidbody.AddForce (new Vector2 (0, _yspeed * _playerSpeed));
-            } else {
-                //normal click
-            }
-            lastClickTime = Time.time;
-        }
-
-            Vector3 screenPosition = Input.mousePosition;
-//            screenPosition = new Vector3(screenPosition.x, 0, 0);
-            _startPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-
-        return _startPosition;
-    }
-
 
 	void OnCollisionEnter2D(Collision2D col) {
 		if (col.gameObject.CompareTag ("Obstacle")) {
@@ -193,5 +125,4 @@ public class PlayerMovement : MonoBehaviour {
 	void SetMenuActive() {
 		_menu.SetActive(true);
 	}
-
 }
