364efbe67f00e27a361e25f9eb2030902698c0fb
Peter <pt9386@gmail.com>
2016-08-13 02:19:40 -0400

    Adds MainMenu to the game

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+
+	public RectTransform _playButton;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayGame() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
