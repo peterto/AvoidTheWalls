05e531fe856dbdfc82ad29a019218dea030c32c8
Peter <pt9386@gmail.com>
2016-08-13 02:58:33 -0400

    Adds fade on play button to make it more prominent

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -8,14 +8,30 @@ public class MainMenuController : MonoBehaviour {
 
 
 	public RectTransform _playButton;
+	public Material _bottomPanelMaterial;
+	public Material _middlePanelMaterial;
+
 	// Use this for initialization
 	void Start () {
-	
+		DOTween.Init(false, true, LogBehaviour.ErrorsOnly);
+		Sequence mySequence = DOTween.Sequence();
+		mySequence.Append(_middlePanelMaterial.DOFade (1, 1f));
+		mySequence.Append(_middlePanelMaterial.DOFade (0, 1f));
+		mySequence.SetLoops (-1, LoopType.Yoyo);
+		_middlePanelMaterial.DOColor (Color.white, 1f).SetLoops(-1, LoopType.Yoyo);
+//		_middlePanelMaterial.DOColor (Color.black, 1f).SetLoops(-1, LoopType.Yoyo);
+//		mySequence.Play ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+
+//		_bottomPanelMaterial.DOColor (Color.blue, 1f).SetLoops(-1, LoopType.Yoyo);
+
+//		_bottomPanelMaterial.DOFade(1, 1f).SetLoops(-1, LoopType.Yoyo);
+
+//		cubeA.DOMove(new Vector3(-2, 2, 0), 1).SetRelative().SetLoops(-1, LoopType.Yoyo);
 	}
 
 	public void PlayGame() {
